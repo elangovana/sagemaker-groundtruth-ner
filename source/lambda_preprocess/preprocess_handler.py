e36479e0e65cb7f4922ec57eead55f9b5969161c
aeg <aeg@amazon.com>
2018-12-21 17:04:06 +1100

    fix preprocessor

--- source/lambda_preprocess/preprocess_handler.py
+++ source/lambda_preprocess/preprocess_handler.py
@@ -3,6 +3,7 @@ def lambda_handler(event, context):
     text = event['dataObject']['source']
     # TODO: use nltk tokeniser..
     tokens = text.split(" ")
+    #TODO: Include token position within text
     token_values = [{"id": i, "value": val} for i, val in enumerate(tokens)]
     result = {
         "taskInput": {"tokens": token_values, "text": text},
